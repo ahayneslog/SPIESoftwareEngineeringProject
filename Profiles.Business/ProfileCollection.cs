4361d27773a3d98fd02d5bbe28cca25962ef872d
Andrew Haynes <andrew.c.haynes.cs@gmail.com>
2019-11-29 17:57:22 -0800

    Bug Fix #2: When clicking a profile, the user is always hitting the first profile in the Profile Collection. Fixed "GetProfile" to not use ProfileList.First(). It now does a naive linear search to match the ID input to the ID of a profile that exists in the ProfileList. I left comments of how to scale this search in the future once profile count is exponentially high.

--- Profiles.Business/ProfileCollection.cs
+++ Profiles.Business/ProfileCollection.cs
@@ -58,11 +58,36 @@ namespace Profiles.Business
             };
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ID">Associated ID to User</param>
+        /// <returns>If ID was valid, a profile. Otherwise, a null value.</returns>
         public Profile GetProfile(int ID)
-        {
-            return ProfileList.First();
+        {            
+            //Solution #1: This approach allows us to avoid validation on ID value
+            //Current Runtime: O(n)
+            //However, if O(ProfileList) becomes exponentially large,
+            //the search needs to switch to a binary search.
+            //We really don't want to deal with a linear search where the ID's value is 1,000,000.
+            //A cleaner solution would be to do the following:
+            //Solution #2: The assumption in this scenario is that ProfileList is sorted
+            //We can also safely assume that the situation is a "write once, read often"
+            //where a user is doing multiple searches. 
+            //So do a sort once, then binary search often.
+            //Another situation to consider is checking if new profiles came in. 
+            //Resort when necessary.
+            //Sort: Insertion Sort
+            //Search: Binary Search
+            //Solution #3: A direct query to database by sanitizing the input of ID.
+            //This also reduces data on mobile device instead of 
+            //hosting irrelevant profile information for searching
+            Profile foundProfile = null;            
+            foreach(Profile profile in ProfileList)
+            {
+                if (profile.ID == ID) foundProfile = profile;
+            }
+            return foundProfile;
         }
-
     }
 }
